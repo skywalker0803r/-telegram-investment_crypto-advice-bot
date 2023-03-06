011558b766ad37010cfed6a31e7fe5bc4370614f
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2023-03-06 14:12:22 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -38,63 +38,8 @@ def send_to_telegram(message):
         print(response.text)
     except Exception as e:
         print(e)
-
-# 定義漲跌訊號函數
-def get_signal(
-      pair='BTCUSDT',
-      freq='15m',
-      n_bar = 10000,
-      client = client
-      ):
-  # get the pair ohlcv data
-  ohlcv = finlab_crypto.crawler.get_nbars_binance(pair,freq,n_bar,client)
-  
-  # def base sma_Strategy
-  @Strategy(sma1=20, sma2=60)
-  def sma_strategy(ohlcv):
-    close = ohlcv.close
-    # crete sma1 and sma2
-    sma1 = close.rolling(sma_strategy.sma1).mean()
-    sma2 = close.rolling(sma_strategy.sma2).mean()
-    # def entries and exits
-    entries = (sma1 > sma2) & (sma1.shift() < sma2.shift())
-    exits = (sma1 < sma2) & (sma1.shift() > sma2.shift())
-    return entries, exits
-  
-  # search range
-  variables = {
-      'sma1': np.arange(5, 100, 5), 
-      'sma2': np.arange(10, 200, 5),
-      }
-  
-  # sma_strategy.backtest
-  portfolio = sma_strategy.backtest(ohlcv, variables=variables, freq=freq ,plot=False)
-  # find portfolio.total_profit().max()
-  temp = portfolio.total_profit()[portfolio.total_profit()==portfolio.total_profit().max()].to_frame().reset_index()
-  # find portfolio.total_profit().max() use n1 and n2
-  n1,n2 = temp['sma1'].values[0],temp['sma2'].values[0]
-  
-  # create signal table
-  table = pd.DataFrame()
-  table['close'] = ohlcv.close
-  table['n1'] = ohlcv.close.rolling(n1).mean()
-  table['n2'] = ohlcv.close.rolling(n2).mean()
-  table['buy'] = ((table['n1'] > table['n2']) & (table['n1'].shift() < table['n2'].shift())).astype(int)
-  table['sell'] = ((table['n1'] < table['n2']) & (table['n1'].shift() > table['n2'].shift())).astype(int)
-  # get last row of the table
-  table = table.replace(0,np.nan).tail(1)
-  # if buy and sell eqal np.nan pass
-  if table[['buy','sell']].sum().sum() == 0:
-     signal = 'PASS'
-  # if buy == 1
-  if table[['buy']].values[0][0] == 1:
-     signal = 'BUY'
-  # if sell == 1
-  if table[['sell']].values[0][0] == 1:
-     signal = 'SELL'
-  return signal,n1,n2
-
-def get_signal_fast(
+        
+def get_sma_signal(
       pair='BTCUSDT',
       freq='15m',
       n_bar = 10000,
@@ -102,11 +47,7 @@ def get_signal_fast(
       n1 = 65,
       n2 = 95,
       ):
-  
-  # get data
   ohlcv = finlab_crypto.crawler.get_nbars_binance(pair,freq,n_bar,client)
-  
-  # get signal table
   table = pd.DataFrame()
   table['close'] = ohlcv.close
   table['n1'] = ohlcv.close.rolling(n1).mean()
@@ -114,17 +55,12 @@ def get_signal_fast(
   table['buy'] = ((table['n1'] > table['n2']) & (table['n1'].shift() < table['n2'].shift())).astype(int)
   table['sell'] = ((table['n1'] < table['n2']) & (table['n1'].shift() > table['n2'].shift())).astype(int)
   table = table.replace(0,np.nan).tail(1)
-  
-  # if buy and sell eqal np.nan pass
   if table[['buy','sell']].sum().sum() == 0:
      signal = 'PASS'
-  # if buy == 1
   if table[['buy']].values[0][0] == 1:
      signal = 'BUY'
-  # if sell == 1
   if table[['sell']].values[0][0] == 1:
      signal = 'SELL'
-  
   return signal,n1,n2
 
 # RSI策略 2023/03/06研發 報酬率15% 最大回撤-0.08%
@@ -134,11 +70,7 @@ def get_rsi_signal(
       n_bar = 10000,
       period = 14,
       client = client):
-    
-    # get data
     ohlcv = finlab_crypto.crawler.get_nbars_binance(pair,freq,n_bar,client)
-  
-    # create signal table
     table = pd.DataFrame()
     table['rsi'] = TA.RSI(ohlcv,period=period)
     table['buy'] = table['rsi'] <= 20 
